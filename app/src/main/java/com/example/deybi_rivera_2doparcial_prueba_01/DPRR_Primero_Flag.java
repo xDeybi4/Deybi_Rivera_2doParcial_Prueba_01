95211a163fcbda3830bc19f3ffda6a95a64775bd
xDeybi4 <deybirivera4@outlook.es>
2022-01-10 18:50:45 -0500

    Segundo commit 18:50

--- app/src/main/java/com/example/deybi_rivera_2doparcial_prueba_01/DPRR_Primero_Flag.java
+++ app/src/main/java/com/example/deybi_rivera_2doparcial_prueba_01/DPRR_Primero_Flag.java
@@ -1,51 +1,121 @@
 package com.example.deybi_rivera_2doparcial_prueba_01;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import android.view.View;
+import com.example.deybi_rivera_2doparcial_prueba_01.eventhandlers.PreferenceChangeListener;
+import com.example.deybi_rivera_2doparcial_prueba_01.lifecyclehelpers.QuizViewModel;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.deybi_rivera_2doparcial_prueba_01.databinding.ActivityDprrPrimeroFlagBinding;
+import java.util.prefs.PreferenceChangeListener;
 
 public class DPRR_Primero_Flag extends AppCompatActivity {
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+    private boolean deviceIsPhone = true;
+    private boolean preferencesChanged = true;
+    private FirstFragment quizFragment;
+    private QuizViewModel quizViewModel;
+    private OnSharedPreferenceChangeListener preferencesChangeListener;
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityDprrPrimeroFlagBinding binding;
+    private void setSharedPreferences() {
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+    }
+
+    private void screenSetUp() {
+        if (getScreenSize() == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                getScreenSize() == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            deviceIsPhone = false;
+        }
+        if (deviceIsPhone) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityDprrPrimeroFlagBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dprr_primero_flag);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        this.quizViewModel = ViewModelProviders.of(this).get(QuizViewModel.class);
+        this.preferencesChangeListener = new PreferenceChangeListener(this);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        this.setSharedPreferences();
+        this.screenSetUp();
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_dprr_primero_flag);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    protected void onStart() {
+        super.onStart();
+        if (preferencesChanged) {
+            this.quizFragment = (FirstFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.quizFragment);
+            this.quizViewModel.setGuessRows(PreferenceManager.getDefaultSharedPreferences(this)
+                    .getString(CHOICES, null));
+            this.quizViewModel.setRegionsSet(PreferenceManager.getDefaultSharedPreferences(this)
+                    .getStringSet(REGIONS, null));
+
+            this.quizFragment.resetQuiz();
+
+            preferencesChanged = false;
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
+        return super.onOptionsItemSelected(item);
+    }
+
+    public int getScreenSize() {
+        return getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+    }
+
+    public MainActivityFragment getQuizFragment() {
+        return this.quizFragment;
+    }
+
+    public QuizViewModel getQuizViewModel() {
+        return quizViewModel;
+    }
+
+    public static String getCHOICES() {
+        return CHOICES;
+    }
+
+    public static String getREGIONS() {
+        return REGIONS;
+    }
+
+    public void setPreferencesChanged(boolean preferencesChanged) {
+        this.preferencesChanged = preferencesChanged;
+    }
+
+
 }
