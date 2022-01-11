c6c9f492c03ab459639f58d8e3575ba88337c7ea
xDeybi4 <deybirivera4@outlook.es>
2022-01-10 19:00:22 -0500

    Tercer commit 19:00

--- app/src/main/java/com/example/deybi_rivera_2doparcial_prueba_01/FirstFragment.java
+++ app/src/main/java/com/example/deybi_rivera_2doparcial_prueba_01/FirstFragment.java
@@ -11,7 +11,8 @@ import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
